a06492c84f0d64ce0f9316507b1bdbef997f8003
ADMIN <bsprasanna123@gmail.com>
2021-10-11 12:08:28 +0530

    msg

--- wordcountdf.py
+++ wordcountdf.py
@@ -15,9 +15,9 @@ class WordExtractingDoFn(beam.DoFn):
     """Returns an iterator over the words of this element.
     The element is a line of text.  If the line is blank, note that, too.
     Args:
-      element: the element being processed
+      element: the element being processed. Ohhhh okay
     Returns:
-      The processed element.
+      The processed element. WOWWW Noice
     """
     return re.findall(r'[\w\']+', element, re.UNICODE)
 
